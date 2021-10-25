23f9408e27db491ad686805642c51d895f820eac
MateuszBuchner <61974282+MateuszBuchner@users.noreply.github.com>
2021-10-25 10:20:54 +0200

    Add files via upload

---
+++ 71/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <ifstream>
+
+double f(double dane [4][5],double x){
+	double wynik = 0;
+	wynik = double[0[1]+(double[0][2])*(double x)
+	else if(x > 1 && < 2){
+	wynik = double[0[1]+(double[0][3])*(double x)
+	else if(x > 1 && < 3){
+	wynik = double[0[1]+(double[0][4])*(double x)
+	else if(x > 1 && < 4){
+	wynik = double[0[1]+(double[0][4])*(double x)
+	}
+}
+
+
+
+int main(int argc, char** argv) {
+	ifstream plik;
+	plik.open("funkcja.txt")
+	
+	int i = 0;
+	doube a,b,c,d;
+	
+	while(plik>>a>>b>>c>>d){
+		dane[i][0];
+		dane[i][1];
+		dane[i][2];
+		dane[i][3];
+		i++;
+	}
+	
+	
+	double mx, mfx;
+	void maksimum(double step){
+	 double x = 4.0;
+	 mx = x;
+	 mfx = ((A*x+B)*x+C)*x+D;
+	 for (x = 4.0; x<5.0; x+=step){
+	 double fx = ((A*x+B)*x+C)*x+D;
+	 if ( fx > mfx ){
+	 mx = x;
+	 mfx = fx;
+}
+}
+} 
+	return 0;
+}
